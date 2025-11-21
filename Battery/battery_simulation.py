fafcbfbd979d42ea0231937af38cb110d238fffb
Leduc Marie <marie.leduc@student-cs.fr>
2025-11-21 10:57:23 +0100

    refactor: change variables' names

--- Battery/battery_simulation.py
+++ Battery/battery_simulation.py
@@ -6,11 +6,11 @@ import pyomo.environ as pyo
 
 @dataclass
 class BatteryParams:
-    eta_ch: float = 1.0
-    eta_dis: float = 1.0
+    CHARGE_YIELD: float = 1.0
+    DISCHARGE_YIELD: float = 1.0 
     dt: float = 1.0
-    SOC_min: float = 0.1
-    SOC_max: float = 0.95
+    SOC_MIN: float = 0.1
+    SOC_MAX: float = 0.95
 
 class BatteryBlock:
     """
@@ -28,13 +28,12 @@ class BatteryBlock:
     def add_variables(self):
         """
         Function to add the operational variables for the BESS
-
         """
         m = self.m
-        if not hasattr(m, 'P_ch'):
-            m.P_ch  = pyo.Var(self.T, domain=pyo.NonNegativeReals)
-        if not hasattr(m, 'P_dis'):
-            m.P_dis = pyo.Var(self.T, domain=pyo.NonNegativeReals)
+        if not hasattr(m, 'PWR_CHARGE'):
+            m.PWR_CHARGE  = pyo.Var(self.T, domain=pyo.NonNegativeReals)
+        if not hasattr(m, 'PWR_DISCHARGE'):
+            m.PWR_DISCHARGE = pyo.Var(self.T, domain=pyo.NonNegativeReals)
         if not hasattr(m, 'SOC'):
             m.SOC   = pyo.Var(self.T, domain=pyo.NonNegativeReals)
 
@@ -48,26 +47,26 @@ class BatteryBlock:
         def soc_dyn_rule(m, t):
             if t == T[-1]:
                 return pyo.Constraint.Skip
-            return m.SOC[t+1] == m.SOC[t] + p.eta_ch*m.P_ch[t]*p.dt - (1.0/p.eta_dis)*m.P_dis[t]*p.dt
+            return m.SOC[t+1] == m.SOC[t] + p.CHARGE_YIELD*m.PWR_CHARGE[t]*p.dt - (1.0/p.DISCHARGE_YIELD)*m.PWR_DISCHARGE[t]*p.dt
         m.SOCdyn = pyo.Constraint(self.T, rule=soc_dyn_rule)
 
         # SOC's bounds
         def soc_lower_bound_rule(m, t):
-            return m.SOC[t] >= p.SOC_min * m.E_bat_max
+            return m.SOC[t] >= p.SOC_MIN * m.EN_BAT_MAX
         m.SOCLowerBound = pyo.Constraint(self.T, rule=soc_lower_bound_rule)
 
         def soc_upper_bound_rule(m, t):
-            return m.SOC[t] <= p.SOC_max * m.E_bat_max
+            return m.SOC[t] <= p.SOC_MAX * m.EN_BAT_MAX
         m.SOCUpperBound = pyo.Constraint(self.T, rule=soc_upper_bound_rule)
 
         # Power constraints
-        def p_ch_limit_rule(m, t):
-            return m.P_ch[t] <= m.P_bat_max
-        m.PchLimit = pyo.Constraint(self.T, rule=p_ch_limit_rule)
+        def pwr_charge_limit_rule(m, t):
+            return m.PWR_CHARGE[t] <= m.PWR_BAT_MAX
+        m.PchLimit = pyo.Constraint(self.T, rule=pwr_charge_limit_rule)
 
-        def p_dis_limit_rule(m, t):
-            return m.P_dis[t] <= m.P_bat_max
-        m.PdisLimit = pyo.Constraint(self.T, rule=p_dis_limit_rule)
+        def pwr_discharge_limit_rule(m, t):
+            return m.PWR_DISCHARGE[t] <= m.PWR_BAT_MAX
+        m.PdisLimit = pyo.Constraint(self.T, rule=pwr_discharge_limit_rule)
 
         # Cycle condition
         def soc_cycle_rule(m):
--- Costs/cost_functions.py
+++ Costs/cost_functions.py
@@ -6,43 +6,43 @@ import pyomo.environ as pyo
 
 @dataclass
 class EconomicParams:
-    c_el: float = 700_000.0 # €/MW_el
-    c_bat_P: float = 150.0 # €/MW
-    c_bat_E: float = 10_000.0 # €/MWh
-    prix_a_terme: float = 75.0 # €/MWh
-    c_CO2: float = 80.0 # €/tCO2
-    alpha: float = 0.07 * (1+0.07)**15 / ((1+0.07)**15 - 1) # Annuity (r=7%, N=15)
+    ELECTROLYSER_CAPEX: float = 700_000.0 # €/MW_el
+    CAPEX_PWR_BAT: float = 150.0 # €/MW
+    CAPEX_EN_BAT: float = 10_000.0 # €/MWh
+    FORWARD_PRICE: float = 75.0 # €/MWh
+    CO2_COST: float = 80.0 # €/tCO2
+    ALPHA: float = 0.07 * (1+0.07)**15 / ((1+0.07)**15 - 1) # Annuity (r=7%, N=15)
 
 class ObjectiveBuilder:
     """
     Builds the objective : annualised CAPEX + electricity cost + CO2
     """
-    def __init__(self, m: pyo.ConcreteModel, T_set, econ: EconomicParams, prix_elec, intensite_co2, dt: float = 1.0, include_co2: bool = False):
+    def __init__(self, m: pyo.ConcreteModel, T_set, ECON_PARA: EconomicParams, ELEC_PRICE, CO2_INTENSITY, dt: float = 1.0, include_co2: bool = False):
         self.m = m
         self.T = T_set
-        self.econ = econ
-        self.prix_elec = prix_elec
-        self.intensite_co2 = intensite_co2
+        self.ECON_PARA = ECON_PARA
+        self.ELEC_PRICE = ELEC_PRICE
+        self.CO2_INTENSITY = CO2_INTENSITY
         self.dt = dt
         self.include_co2 = include_co2
 
     def capex_annual(self):
-        m, e = self.m, self.econ
-        return e.alpha * (e.c_bat_P * m.P_bat_max + e.c_bat_E * m.E_bat_max)
+        m, e = self.m, self.ECON_PARA
+        return e.ALPHA * (e.CAPEX_PWR_BAT * m.PWR_BAT_MAX + e.CAPEX_EN_BAT * m.EN_BAT_MAX)
 
     def cout_elec(self):
-        m, e, dt, prix = self.m, self.econ, self.dt, self.prix_elec
-        # Forward cost (phi * P_hydro_max) + spot (P_spot)
-        return sum((e.prix_a_terme * m.phi * 1.0 * dt + prix[t] * m.P_spot[t] * dt) for t in self.T)
+        m, e, dt, prix = self.m, self.ECON_PARA, self.dt, self.ELEC_PRICE
+        # Forward cost (PHI * PWR_H2_MAX) + spot (PWR_SPOT)
+        return sum((e.FORWARD_PRICE * m.PHI * 1.0 * dt + prix[t] * m.PWR_SPOT[t] * dt) for t in self.T)
 
     def emissions_co2(self):
-        m, dt, I = self.m, self.dt, self.intensite_co2
-        return sum(I[t] * m.P_spot[t] * dt for t in self.T)
+        m, dt, I = self.m, self.dt, self.CO2_INTENSITY
+        return sum(I[t] * m.PWR_SPOT[t] * dt for t in self.T)
 
     def build_objective(self):
-        m, e = self.m, self.econ
+        m, e = self.m, self.ECON_PARA
         base_cost = self.capex_annual() + self.cout_elec()
         if self.include_co2:
-            base_cost += e.c_CO2 * self.emissions_co2()
+            base_cost += e.CO2_COST * self.emissions_co2()
         m.Obj = pyo.Objective(expr=base_cost, sense=pyo.minimize)
         return m.Obj
